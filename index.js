55ee1357eb0748234d4fd39f185c3f45568a43ea
Ishaan <ifarooq20@gmail.com>
2019-08-24 13:56:21 +0530

    Added functionality
    Added options to reply to each memo via mail.
    
    Added option to copy memo to clipboard.
    
    UI canges (custom scrollbar, new bttons, etc)

--- index.js
+++ index.js
@@ -8,9 +8,30 @@ const adminElems = document.querySelectorAll('.admin-logged-in');
 const acname = document.querySelector('.acname');
 const acemail = document.querySelector('.acemail');
 let curUser = undefined;
+curUserEmail = undefined;
+const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+
+const copy = (copyText) => {
+//   copyText.select();
+
+var textArea = document.createElement("textarea");
+  textArea.value = copyText;
+  textArea.style.visibility = 'none';
+  document.body.appendChild(textArea);
+  textArea.focus();
+  textArea.setSelectionRange(0, 99999);
+  textArea.select();
+  document.execCommand("copy");
+  document.body.removeChild(textArea);
+  console.log(String(copyText));
+//   M.toast({html: 'I am a toast!'})
+  M.toast({html: 'Copied!'})
+}
 
 const UI = (userID) => {
+    
     if (userID) {
+        curUserEmail = userID.email;
         if (userID.admin){
             console.log(userID.admin);
             adminElems.forEach((elem) => {
@@ -32,12 +53,12 @@ const UI = (userID) => {
             accountDetails.innerHTML = ` <div class="row">
             <div class="col s12">
               <div class="card">
-                <div class="card-content">
+                <div class="card-content grey lighten-2">
                 <span class="card-title">Name: ${doc.data().name}</span>
                   <p>Email: ${userID.email}</p>
                   <p>About: ${doc.data().bio}</p>
                 </div>
-                <div class="card-action">
+                <div class="card-action grey lighten-3">
                 <a href="#" class="" id="logout">Logout</a>
                 </div>
               </div>
@@ -71,24 +92,32 @@ const displayNotes = (notes) => {
             const notesContent = doc.data();
             const noteCard = `
             <div class="col s12 m6">
-                <div class="card hoverable indigo lighten-1">
-                    <div class="card-content white-text">
-                        <span class="card-title activator">${notesContent.Title}</span>
+                <div class="card hoverable cardcolor grey lighten-4">
+                    <div class="card-content">
+                        <span class="card-title">${notesContent.Title}</span>
                         <hr>
                         <p class="truncate">${notesContent.TextContent}</p>
-                        <p>By: ${notesContent.By}</p>
+                        
                     </div>
-                    <!-- <div class="card-action">
-                        <a href="#">This is a link</a>
-                        <a href="#">This is a link</a>
-                    </div> -->
-                    <!--
+                    <div class="card-action grey lighten-3 activator">
+                        <a class="indigo-text text-lighten-2 waves-effect waves-light activator"><i class="material-icons right">info</i>Info</a>
+                    </div>
+                    
                     <div class="card-reveal">
-                        <span class="card-title grey-text text-darken-4">Card Title<i class="material-icons right">close</i></span>
-                        <p>Here is some ajdoijs sakdj ofsjoij oijf o osiajdf  j sfj aoijfoisjefse jaskjdfh 
-                        ihsdfsfyfytdt   more information about this product that is only revealed once clicked on.</p>
+                        <span class="card-title grey-text text-darken-4">${notesContent.Title}<i class="material-icons right">close</i></span>
+                        <p>By: ${notesContent.By}</p>
+                    <p>On: ${notesContent.Timeofnote.toDate().toLocaleDateString("en-US", options)}</p>
+                    <div class="center-align">
+                        <div class="left">
+                        <a class="waves-effect waves-light btn-small red lighten-1" 
+                            href="mailto:${curUserEmail}?subject=Replying%20for%20memo:%20${notesContent.Title}
+                            &body=Memo%20Body:%20${notesContent.TextContent}
+                            %0APublished%20at:%20${notesContent.Timeofnote.toDate().toLocaleDateString("en-US", options)}
+                            %0A*******TYPE YOUR REPLY BELOW THIS LINE*******%0A%0A">
+                        <i class="material-icons right">email</i>reply</a></div>
+                        <div class="right"><a class="waves-effect waves-light btn-small" onclick="copy('${notesContent.TextContent}')"><i class="material-icons right">file_copy</i>copy</a></div>
                     </div>
-                    -->
+                    </div>  
                 </div>
             </div>`;
             cardsHtml += noteCard;
@@ -120,7 +149,8 @@ newNoticeForm.addEventListener('submit', (e) => {
     db.collection('Notes').add({
         Title: newNoticeForm['title'].value,
         TextContent: newNoticeForm['content'].value,
-        By: curUser
+        By: curUser,
+        Timeofnote: firebase.firestore.Timestamp.now()
     }).then(()=>{
         const modal = document.querySelector('#modal-create');
         M.Modal.getInstance(modal).close();
